d5e1eacb9834750572b0f97545340bf449129167
John F <johnrexter.flores@gmail.com>
2025-01-21 08:50:09 +0800

    Fix deleted branches too

--- app/Models/StockHistory.php
+++ app/Models/StockHistory.php
@@ -29,6 +29,6 @@ class StockHistory extends Model
 
     public function branch()
     {
-        return $this->belongsTo(Branch::class);
+        return $this->belongsTo(Branch::class)->withTrashed();
     }
 }
